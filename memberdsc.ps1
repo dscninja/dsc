e6b5418725d5c4a5cd62b924006ad3ab40735d47
dscninja <jstrange@LT1170.skyline.gb>
2015-12-17 10:35:38 -0600

    Commit

---
+++ memberdsc.ps1
@@ -0,0 +1,22 @@
+﻿Configuration Member {
+        param 
+	    ( 
+	        [Parameter(Mandatory)]
+	        [String]$DomainName,
+
+	        [Parameter(Mandatory)]
+	        [System.Management.Automation.PSCredential]$Admincreds
+	    )
+
+        Import-DscResource -ModuleName xComputerManagement
+                    
+		[System.Management.Automation.PSCredential ]$pass = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+        
+        Node Localhost {
+           xComputer JoinDomain {
+                Name = $env:COMPUTERNAME
+                DomainName = $DomainName
+                Credential = $pass
+           }
+        }
+}
