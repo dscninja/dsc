9d399467b4ea54fad5b60347e4183e717757cf17
dscninja <jstrange@LT1170.skyline.gb>
2015-12-17 16:50:16 -0600

    Commit

---
+++ AzureNSG.ps1
@@ -0,0 +1,3 @@
+﻿Get-AzureNetworkSecurityGroup -Name NSG-BackEnd -Detailed
+
+Get-AzureNetworkSecurityGroup -Name NSG-BackEnd | Set-AzureNetworkSecurityRule -Name "ssh" -Action Allow -Protocol "TCP" -Type Inbound -Priority 110 -SourceAddressPrefix Internet -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange '22'
